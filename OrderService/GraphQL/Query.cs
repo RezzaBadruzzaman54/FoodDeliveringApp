b1aa0a4e9d24af0f84b2f0d2bcd18339ed41ed68
rezzabadruzzaman54 <rezzabadruzzaman54@gmail.com>
2022-05-21 04:39:30 +0700

    Menambahkan Tracking Order By Buyer

--- OrderService/GraphQL/Query.cs
+++ OrderService/GraphQL/Query.cs
@@ -1,5 +1,6 @@
 ﻿using FoodDeliveringDomain.Models;
 using HotChocolate.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace OrderService.GraphQL
@@ -24,5 +25,31 @@ namespace OrderService.GraphQL
         public IQueryable<Order> GetManagerOrders([Service] FoodDeliveringAppContext context) =>
           context.Orders;
 
+        [Authorize(Roles = new[] { "BUYER" })]
+        public IQueryable<OrderTracking> GetOrderTrackings([Service] FoodDeliveringAppContext context, ClaimsPrincipal claimsPrincipal)
+        {
+            List<OrderTracking> orderTrackings = new();
+            var username = claimsPrincipal.Identity.Name;
+
+            var user = context.Users.FirstOrDefault(u => u.UserName == username);
+            if (user == null) return orderTrackings.AsQueryable();
+
+            var tracking = context.OrderTrackings.ToList();
+            //var orders = context.Buyers.Include(b => b.Orders).ThenInclude(o => o.OrderTrackings).ToList();
+            var orders = context.Orders.Include(o => o.Buyer).Include(o => o.OrderTrackings)
+                .Where(o => o.Buyer.UserId == user.Id && o.Id == tracking[0].OrderId).ToList();
+
+            foreach (var order in orders)
+            {
+                if (order.Status == StatusOrder.OnDelivery)
+                {
+                    foreach (var orderTracking in order.OrderTrackings)
+                    {
+                        orderTrackings.Add(orderTracking);
+                    }
+                }
+            }
+            return orderTrackings.AsQueryable();
+        }
     }
 }
